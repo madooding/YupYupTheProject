81ca768146ea862b9bde757069956a60670df41d
madooding <madooding@yahoo.co.th>
2015-03-10 01:11:10 +0700

    add randint function

--- test.c
+++ test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int randint(int);
 
 int main(){
-	printf("Hello, World.");
+    printf("Hello, world");
 	return 0;
+}
+
+int randint(int size){
+	int num;	
+    time_t t;
+	srand((unsigned) time(&t));
+	return rand() % size;
 }
