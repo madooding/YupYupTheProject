4924590bcfab75a910f440fdd2acef95c8aa7284
madooding <madooding@yahoo.co.th>
2015-03-11 01:06:12 +0700

    add more function

--- test.c
+++ test.c
@@ -3,9 +3,13 @@
 #include <time.h>
 
 int randint(int);
+void perchar(char text[]);
 
 int main(){
-    printf("Hello, world");
+    char text[30];
+    gets(text);
+    perchar(text);
+    printf("%s\n", text);
 	return 0;
 }
 
@@ -15,3 +19,13 @@ int randint(int size){
 	srand((unsigned) time(&t));
 	return rand() % size;
 }
+void perchar(char text[]){
+     int i, ranNumber;
+     char temp;
+     for(i = 0; i < strlen(text); i++){
+           ranNumber = randint(strlen(text));
+           temp = text[i];
+           text[i] = text[ranNumber];
+           text[ranNumber] = temp;
+           }
+     }
